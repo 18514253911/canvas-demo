5a93acde2c80cef09b4850af667438927fce7d38
yanchanglu <513894105@qq.com>
2016-12-04 11:26:20 +0800

    update data

---
+++ countdown.js
@@ -0,0 +1,81 @@
+/**
+ * Created by ycl on 2016/12/4.
+ */
+var WINDOW_WIDTH=1024;
+var WINDOW_HEIGHT=768;
+var MARGIN_LEFT=30;
+var MARGIN_TOP=60;
+var RADIUS=8;
+var curShowTimeSeconds=0;
+var endTime=new Date(2016,11,6,12,12);
+window.onload=function(){
+    var canvas=document.getElementById("canvas");
+    var context=canvas.getContext("2d");
+
+    canvas.height=WINDOW_HEIGHT;
+    canvas.width=WINDOW_WIDTH;
+
+    curShowTimeSeconds=getCurShowTimeSeconds();
+    setInterval(function(){
+        render(context);
+        update();
+    },50);
+
+
+
+};
+function update(){
+    var nextShowTimeSeconds=getCurShowTimeSeconds();
+    var nextHours=parseInt(nextShowTimeSeconds/3600);
+    var nextMinutes=parseInt((nextShowTimeSeconds-nextHours*3600)/60);
+    var nextSeconds=nextShowTimeSeconds%60;
+
+    var hours=parseInt(curShowTimeSeconds/3600);
+    var minutes=parseInt((curShowTimeSeconds-hours*3600)/60);
+    var seconds=curShowTimeSeconds%60;
+
+    if (nextSeconds!==seconds){
+        curShowTimeSeconds=nextShowTimeSeconds;
+    }
+
+};
+function getCurShowTimeSeconds() {
+    var curTime=new Date();
+    var ret=endTime.getTime()-curTime.getTime();
+    ret=Math.round(ret/1000);
+    return ret>=0?ret:0;
+};
+function render(ext) {
+    ext.clearRect(0,0,WINDOW_WIDTH,WINDOW_HEIGHT);
+    var hours=parseInt(curShowTimeSeconds/3600);
+    var minutes=parseInt((curShowTimeSeconds-hours*3600)/60);
+    var seconds=curShowTimeSeconds%60;
+
+    renderDigit(MARGIN_LEFT,MARGIN_TOP,parseInt(hours/10),ext);
+    renderDigit(MARGIN_LEFT+15*(RADIUS+1),MARGIN_TOP,parseInt(hours%10),ext);
+    renderDigit(MARGIN_LEFT+30*(RADIUS+1),MARGIN_TOP,10,ext);
+
+    renderDigit(MARGIN_LEFT+39*(RADIUS+1),MARGIN_TOP,parseInt(minutes/10),ext);
+    renderDigit(MARGIN_LEFT+54*(RADIUS+1),MARGIN_TOP,parseInt(minutes%10),ext);
+    renderDigit(MARGIN_LEFT+69*(RADIUS+1),MARGIN_TOP,10,ext);
+
+    renderDigit(MARGIN_LEFT+78*(RADIUS+1),MARGIN_TOP,parseInt(seconds/10),ext);
+    renderDigit(MARGIN_LEFT+93*(RADIUS+1),MARGIN_TOP,parseInt(seconds%10),ext);
+
+
+};
+function renderDigit(x,y,num,ext) {
+    ext.fillStyle="rgb(0,102,153)";
+
+    for (var i=0;i<digit[num].length;i++){
+        for(var j=0;j<digit[num][i].length;j++){
+            if(digit[num][i][j]===1){
+                ext.beginPath();
+                ext.arc(x+j*2*(RADIUS+1)+(RADIUS+1),y+i*2*(RADIUS+1)+(RADIUS+1),RADIUS,0,2*Math.PI);
+                ext.closePath();
+
+                ext.fill();
+            }
+        }
+    }
+}
